c2e67137e81f5ca9aa01bf9004bda17b28dd525c
Marcos Kubis <marcoskubis@gmail.com>
2018-07-09 11:44:46 -0300

    Some tests added

--- tests/SearchableTest.php
+++ tests/SearchableTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tests;
 
+use Tests\Category;
+use Tests\Product;
 use Tests\ProductSearch;
 
 class SearchableTest extends TestCase
@@ -8,18 +10,56 @@ class SearchableTest extends TestCase
 
     /** @test */
     function it_can_get_all() {
+
+        $products = $this->factory->of(Product::class)->times(10)->create();
+
         $all = ProductSearch::make()->get();
 
-        $this->assertEquals($this->products->count(), $all->count());
+        $this->assertEquals($products->count(), $all->count());
     }
 
     /** @test */
     function it_can_filter_by_name() {
-        $product = $this->products->last();
+        $products = $this->factory->of(Product::class)->times(10)->create();
+        $product = $products->last();
 
         $all = ProductSearch::make()->filter(['name' => $product->name])->get();
 
         $this->assertEquals(1, $all->count());
         $this->assertEquals($product->name, $all->first()->name);
+    }
+
+    /** @test */
+    function it_can_sort_by_name() {
+        $products = $this->factory->of(Product::class)->times(10)->create();
+        $firstProduct = $products->sortBy('name')->first();
+        $lastProduct = $products->sortByDesc('name')->first();
+
+
+        $all = ProductSearch::make()->sorting(['name' => 'asc'])->get();
+
+        $this->assertEquals($firstProduct->name, $all->first()->name);
+        $this->assertEquals($lastProduct->name, $all->last()->name);
+        $this->assertEquals(10, $all->count());
+    }
+
+    /** @test */
+    function it_can_sort_relation_column_by_asc() {
+        $products = $this->factory->of(Product::class)->times(10)->create();
+        $firstCategory = Category::orderBy('name', 'asc')->first();
+
+        $all = ProductSearch::make()->sorting(['category.name' => 'asc'])->get();
+
+        $this->assertEquals($firstCategory->name, $all->first()->category->name);
+    }
+
+    /** @test */
+    function it_can_sort_relation_column_by_desc() {
+        $products = $this->factory->of(Product::class)->times(10)->create();
+        $firstCategory = Category::orderBy('name', 'desc')->first();
+
+        $all = ProductSearch::make()->sorting(['category.name' => 'desc'])->get();
+
+        $this->assertEquals($firstCategory->name, $all->first()->category->name);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,9 @@ use Kubis\Filtering\Searchable;
 
 abstract class TestCase extends BaseTestCase
 {
+
+    protected $factory;
+
     public function setUp()
     {
         $capsule = new Manager;
@@ -19,28 +22,55 @@ abstract class TestCase extends BaseTestCase
         $capsule->setAsGlobal();
         $capsule->bootEloquent();
 
+        $this->factory = new Factory(\Faker\Factory::create());
+
+        $this->migrate();
+        $this->factories();
+    }
+
+    private function migrate()
+    {
         Manager::schema()->dropIfExists('products');
+        Manager::schema()->dropIfExists('categories');
+        Manager::schema()->create('categories', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
         Manager::schema()->create('products', function ($table) {
             $table->increments('id');
             $table->string('name');
             $table->string('description');
+            $table->integer('category_id');
             $table->timestamps();
         });
+    }
 
-        $factory = new Factory(\Faker\Factory::create());
-        $factory->define(Product::class, function (\Faker\Generator $faker) {
+    private function factories()
+    {
+        $this->factory->define(Category::class, function (\Faker\Generator $faker) {
+            return [
+                'name' => $faker->name,
+            ];
+        });
+        $this->factory->define(Product::class, function (\Faker\Generator $faker) {
             return [
                 'name' => $faker->name,
                 'description' => $faker->sentence,
+                'category_id' => $this->factory->of(Category::class)->create()->id
             ];
         });
-
-        $this->products = $factory->of(Product::class)->times(10)->create();
     }
 }
 
 
-class Product extends Model {}
+class Product extends Model {
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
+class Category extends Model {}
 
 class ProductSearch extends Searchable
 {
